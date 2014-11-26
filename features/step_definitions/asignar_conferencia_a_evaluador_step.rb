126c27b2d80bf6b5af915d06deddd02380b42865
Martin Carniello <Martin Carniello@gmail.com>
2014-11-25 22:30:43 -0300

    Posible fix al build de Travis.

--- features/step_definitions/asignar_conferencia_a_evaluador_step.rb
+++ features/step_definitions/asignar_conferencia_a_evaluador_step.rb
@@ -1,14 +1,9 @@
 Given(/^me dirijo al detalle de la conferencia con titulo (.+)$/) do |titulo_conferencia|
 	@browser.goto("http://localhost:3000/ver_conferencias")
-	@browser.table.trs[1..-1].detect { |tr| tr.td(index: 0).text == titulo_conferencia }.td(index: 3).click
+	@browser.table.trs[1..-1].detect { |tr| tr.td(index: 0).text == titulo_conferencia }.td(index: 3).link.click
 end
 
 When(/^asigno la conferencia al evaluador (.+)$/) do |evaluador|
-	if @browser.div(id: "backtrace").present?
-		msg = @browser.div(id: "rack").table.tr(index: 41).td(class: "code").div.text
-		@browser.select_list(id: "#{msg}").select_value(evaluador)
-	end
-
 	@browser.select_list(id: "account_evaluador").select_value(evaluador)
 	@browser.button(id: "account_evaluador_boton").click
 end
